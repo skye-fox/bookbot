a3c023f16c08dea7ed793080d932cf30b4c60f75
skye-fox <105951568+skye-fox@users.noreply.github.com>
2024-06-15 21:43:02 -0500

    Refactor get_character_count to create a list of dictionaries, implement start oRefactor get_character_count function and add print_report function

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     word_count = get_word_count(text)
-    print(f"{word_count} words found in the document!")
-    print(get_character_count(text))
+    character_count = get_character_count(text)
+    character_count.sort(reverse=True, key=lambda x: x["count"])
+    print(character_count)
+    print_report(word_count)
 
 
 def get_book_text(path):
@@ -25,13 +27,18 @@ def get_word_count(text):
 def get_character_count(text):
     """Function to count occurances of each letter in the file"""
     lowercase = text.lower()
-    character_count = {}
-    for char in lowercase:
-        if char in character_count:
-            character_count[char] += 1
-        else:
-            character_count[char] = 1
+    character_count = []
+    for char in set(lowercase):
+        if char.isalpha():
+            count = lowercase.count(char)
+            character_count.append({"letter": char, "count": count})
     return character_count
+
+
+def print_report(word_count):
+    """Function to print the file analysis"""
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count} words found in the document")
 
 
 main()
